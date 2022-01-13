de66853d4faa5bc2fe25416096124b3d4355b81b
Kemal Aydık <kemalaydik@gmail.com>
2022-01-14 00:01:08 +0300

    release hiddenSearchWidget

---
+++ hiddenSearchWidget/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const btn = document.querySelector('.btn');
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', () => {
+	input.focus();
+
+	search.classList.toggle('active');
+});
