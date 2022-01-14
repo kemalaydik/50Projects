fbdf63ee3ebda33b9376765090fd17634f03d700
Kemal Aydık <kemalaydik@gmail.com>
2022-01-14 07:26:14 +0300

    blurryLoad

---
+++ blurryLoading/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const bg = document.querySelector('.bg');
+const loadingText = document.querySelector('.loading-text');
+let load = 0;
+let int = setInterval(blurryLoad, 30);
+
+function scale(number, inMin, inMax, outMin, outMax) {
+	return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
+function blurryLoad() {
+	if (load > 99) clearInterval(int);
+	loadingText.innerText = `${load}%`;
+	loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+	bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px`;
+	load++;
+}
