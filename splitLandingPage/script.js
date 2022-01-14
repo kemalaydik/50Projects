00acc605fd57b65996f3ef69c53cba85eefc3039
Kemal Aydık <kemalaydik@gmail.com>
2022-01-14 14:51:52 +0300

    splitLandingPage

---
+++ splitLandingPage/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const splits = document.querySelectorAll('.split');
+splits.forEach(e =>
+	e.addEventListener('mouseover', function () {
+		this.classList.add('active');
+	}),
+);
+splits.forEach(e =>
+	e.addEventListener('mouseleave', function () {
+		this.classList.remove('active');
+	}),
+);
